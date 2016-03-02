6c0aff1f0b741fa789a81bd69a440a135adedbef
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-03-02 16:37:28 +0000

    randomises order of letters to be drawn from bag

--- react_scrabble_frontend/client/src/Bag.js
+++ react_scrabble_frontend/client/src/Bag.js
@@ -30,3 +30,19 @@ englishLetters = [
 "Y", "Y",
 "Z" 
 ];
+
+// Randomize array order using Durstenfeld shuffle algorithm.
+var shuffle = function(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+var Bag = shuffle(englishLetters);
+
+module.exports = Bag;
+
